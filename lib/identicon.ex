02298d6213dc65cbec4a02a87f530ce4baca8d22
Jeremy <mjerem34@protonmail.com>
2017-08-08 11:43:07 +0200

    Initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,78 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter(grid, fn(row) ->
+      {value, _index} = row
+      rem(value, 2) == 0
+    end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map(grid, fn(tuple) ->
+      {_value, index} = tuple
+
+      x_axis = rem(index, 5) * 50
+      y_axis = div(index, 5) * 50
+
+      top_left = {x_axis, y_axis}
+      bottom_right = { x_axis + 50, y_axis + 50}
+
+      {top_left, bottom_right}
+    end)
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
+  end
+end
